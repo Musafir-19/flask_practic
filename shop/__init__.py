ee04f55eb6be3ce8ead7c27e9be15138b51330fb
Musafir-19 <rkhunkarov@mail.ru>
2021-09-05 14:02:09 +0300

    first commit

---
+++ shop/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import Config
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config.from_object(Config)
+migrate = Migrate(app, db)
+
+from shop import routes, models
---
+++ shop/routes.py
@@ -0,0 +1,14 @@
+from shop import app
+from flask import render_template
+from shop.models import Product
+
+
+@app.route('/')
+def index():
+    products = Product.query.all()
+    return render_template('index.html', products=products)
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
